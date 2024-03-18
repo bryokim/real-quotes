487f661004de0da54433b3786acd5cb4b662a28b
Brian Kimathi <bryo.kim1@gmail.com>
2024-03-18 21:50:06 +0300

    feat: add styling for the scrollbar

--- app.vue
+++ app.vue
@@ -3,3 +3,30 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+/* scrollbar */
+
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #000000;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgb(238, 225, 225);
+  border-radius: 50px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(1, 1, 2) 585;
+}
+
+/* end scrollbar */
+</style>
